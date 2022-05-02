4526b541162084cba1f4bd8bb4b7ded53482d7bb
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-05-01 17:49:36 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+        .then(res => res.json())
+        .then(obj => {
+            console.log(obj)
+        document.querySelector('body').innerHTML = `
+        <p>${obj.id}</p>`})
+        .catch(error => {
+            alert('Unauthorized Access')
+            document.body.innerHTML = `
+            <p>${error}.message</p>`})
+
+}
+
+submitData()
+
+
+
+
+
+
+
+
+
+
+
+
+
